9108971c8c13450a8796cdfea12ec9cd6011365e
Vadim Plataniy <vadim.plataniy@gmail.com>
2021-05-03 13:20:24 +0300

    Dev (#3)
    * gitignore fix
    
    * Some refactoring
    
    * Add Events script, fix mouse event delegate
    
    * Decompos. of rendering functional
    
    * Reorganization
    
    * re
    
    * re
    
    * deleted garbage unit testes
    
    * no .idea
    
    * no migration report
    
    * no ReSharper solution setting for unit tests
    
    * gitignore fix
    
    * change native to package references(SFML)

--- Game/MainGame.cs
+++ Game/MainGame.cs
@@ -1,350 +1,277 @@
-﻿using System;
-using System.Collections;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Data;
-using System.IO;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Security;
-using OpenTK.Windowing.Desktop;
-using Rendering;
-using Events;
-using OpenTK.Graphics.ES11;
-using SFML.Graphics;
-using SFML.System;
-using SFML.Window;
+using Game.Rendering;
 
 
-namespace Game
+
+namespace Game.Logic
 {
     
-    
-    public abstract class IPerson
+    public abstract class PersonBase
     {
-        public abstract bool IsBot();
-        protected abstract int id { get;}
-        public abstract IRules Rules { get; set; }
-        public abstract int Score { get; set; }
-
+        public readonly bool IsBot;
+        public readonly int Id;
+        public readonly RulesBase RulesBase;
+        public int Score { get; protected set; }
         public abstract bool MakeStep(int x, int y);
-        internal abstract int PassesCount { get; set; }
+        internal int PassesCount { get; set; }
 
-    }
-    public class Player:IPerson
-    {
-        internal override int PassesCount { get; set; }
-        protected override int id { get; }
-        public override IRules Rules { get; set; }
-        public override int Score { get; set; }
-        public override bool IsBot()
+        protected PersonBase(int id, bool isBot, RulesBase rulesBase)
         {
-            return false;
+            Id = id;
+            IsBot = isBot;
+            RulesBase = rulesBase;
         }
-
-        int MakeStatusStone(int X, int Y, ref Dictionary<int, int[]> Group, ref bool bOpen)
+    }
+    
+    public class Player:PersonBase
+    {
+        private int MakeStatusStone(int x, int y, ref Dictionary<int, int[]> group, ref bool bOpen)
         {
-            int EnemyId = Rules.Matrix[X, Y];
-            Group[(X+Y)*(X+Y+1)/2 + Y] = new int[] { X,Y};
-            FStoneWayInfo[] WaysInfo = Rules.IsSurrounded(X, Y, EnemyId, new[] {-1,-2});
-            foreach (FStoneWayInfo Info in WaysInfo)
+            int enemyId = (int)RulesBase[x, y];
+            group[(x+y)*(x+y+1)/2 + y] = new[] { x,y};
+            StoneWayInfo[] waysInfo = RulesBase.IsSurrounded(x, y, enemyId, new[] {-1,-2});
+            foreach (StoneWayInfo info in waysInfo)
             {
-                switch (Info.WayType)
+                switch (info.WayTypeType)
                 {
-                    case EWays.OPEN:
+                    case WayType.Open:
                         bOpen = true;
                         break;
-                    case EWays.FRIEND:
-                        int HASH = (Info.X + Info.Y) * (Info.X + Info.Y + 1) / 2 + Info.Y;
-                        if (!Group.ContainsKey(HASH)) MakeStatusStone(Info.X,Info.Y,ref Group,ref bOpen);
+                    case WayType.Friend:
+                        int hash = (info.X + info.Y) * (info.X + info.Y + 1) / 2 + info.Y;
+                        if (!group.ContainsKey(hash)) MakeStatusStone(info.X,info.Y,ref group,ref bOpen);
                         break;
                 }
             }
-
-            return EnemyId;
+            return enemyId;
         }
-
-        void FillByKey(Dictionary<int, int[]> Group, int Key)
+        private void FillByKey(Dictionary<int, int[]> group, int key)
         {
-            foreach (int[] Var in Group.Values)
+            foreach (int[] var in group.Values)
             {
-                Rules.Matrix[Var[0], Var[1]] = Key;
+                RulesBase[var[0], var[1]] = key;
             }
         }
-        public void OcupateEnemy()
+        public void OccupyEnemy()
         {
             
-            for (int i = 0; i < Rules.Matrix.GetLength(0); i++)
+            for (int i = 0; i < RulesBase[0]; i++)
             {
-                for (int j = 0; j < Rules.Matrix.GetLength(1); j++)
+                for (int j = 0; j < RulesBase[1]; j++)
                 {
-                    if (Rules.Matrix[i, j] != 0 && Rules.Matrix[i, j] != id 
-                                                &&Rules.Matrix[i,j]>-1)
+                    if (RulesBase[i, j] != 0 && RulesBase[i, j] != Id 
+                                                &&RulesBase[i,j]>-1)
                     {
-                        bool bOpen = false;
-                        Dictionary<int,int[]> Group = new Dictionary<int,int[]>();
-                        int EnemyID = MakeStatusStone(i,j,ref Group,ref bOpen);
-
-                        if (bOpen)
+                        bool isOpen = false;
+                        Dictionary<int,int[]> group = new Dictionary<int,int[]>();
+                        int enemyId = MakeStatusStone(i,j,ref group,ref isOpen);
+                        if (isOpen)
                         {
-                            FillByKey(Group,-2 - EnemyID);
+                            FillByKey(group,-2 - enemyId);
                         }
                         else
                         {
-                            FillByKey(Group,-1);
-                            int Kills = Group.Count;
-                            Score += Kills;
-                            Console.WriteLine("You,MFK, killed "+Kills+" enemy stones/");
-                            /*
-                            FMessage Message = new FMessage();
-                            Message.Data = new ArrayList();
-                            Message.Data.Add(Group);
-                            Notify(this,EEvents.REMOVE_STONE,Message);*/
+                            FillByKey(group,-1);
+                            int kills = group.Count;
+                            Score += kills;
+                            Console.WriteLine("You,MFK, killed "+kills+" enemy stones/");
                         }   
-                        Group.Clear();
-                        //уничтожаем по групам(меняем значение на нейтральное)
-                        //таки образом эти значения не будут трогатся при повторном обходе
+                        group.Clear();
                     } 
                 }
             }
         }
-
-        public void ClearField()
+        private void ClearField()
         {
-            for (int i = 0; i < Rules.Matrix.GetLength(0); i++)
+            for (int i = 0; i < RulesBase[0]; i++)
             {
-                for (int j = 0; j < Rules.Matrix.GetLength(0); j++)
+                for (int j = 0; j < RulesBase[1]; j++)
                 {
-                    if (Rules.Matrix[i, j] == -1) Rules.Matrix[i, j] = 0;
-                    else if (Rules.Matrix[i, j] < -1)
+                    if (RulesBase[i, j] == -1) RulesBase[i, j] = 0;
+                    else if (RulesBase[i, j] < -1)
                     {
-                        Rules.Matrix[i, j] = -(Rules.Matrix[i, j] + 2);
+                        RulesBase[i, j] = -(RulesBase[i, j] + 2);
                     }
                 }    
             }
         }
         public override bool MakeStep(int x, int y)
         {
-            
-            bool bAnswer = Rules.Check(GetHashCode(),x,y);
+            bool bAnswer = RulesBase.Check(Id,x,y);
             if (bAnswer)
             {
-                OcupateEnemy();
+                OccupyEnemy();
                 ClearField();
-             
                 PassesCount = 0;
-                Rules.NextStep();
+                RulesBase.NextStep();
             }
             return bAnswer;
-            
         }
-        
-        //
         public void Pass( )
         {
             PassesCount = 1;
-            Rules.NextStep();
+            RulesBase.NextStep();
         }
         public override int GetHashCode()
         {
-            return id;
+            return base.GetHashCode();
         }
-
         public override bool Equals(object? obj)
         {
-            if (obj.GetType() != typeof(Player)) return false;
-            Player obj2 = (Player) obj;
-            if (!ReferenceEquals(Rules, obj2.Rules)) return false;
+            if (obj?.GetType() != GetType()) return false;
+            Player obj2 = (Player) obj; 
+            if (!Equals(RulesBase, obj2.RulesBase)) return false;
             if (obj2.GetHashCode() != GetHashCode()) return false;
             return true;
         }
-
-        public Player(IRules rules)
+        public Player(RulesBase rulesBase):base(1,false, rulesBase)
         {
-
-            if (Rules == null)
-            {
-                Rules = rules;
-                id = 1;
-                Rules.AddPlayer(this);
-            }
+            RulesBase.AddPlayer(this);
         }
-
-        public Player(IRules rules, int id)
+        public Player(RulesBase rulesBase, int id):base(id,false, rulesBase)
         {
-            if (Rules == null)
-            {
-                Rules = rules;
-                this.id = id;
-                Rules.AddPlayer(this);
-            }
+            RulesBase.AddPlayer(this);
+        }
+    }
+    public abstract class RulesBase
+    {
+        public abstract void NextStep();
+        public abstract void AddPlayer(PersonBase personBase);
+        public int[,] Matrix {private get; set; }
+        
+        
+        
+        protected RulesBase(int size)
+        {
+            Matrix = new int[size,size];
             
         }
-    }
-    public interface IRules
-    {
+        public int this[int x] => Matrix.GetLength(x);
 
-
-        public void NextStep();
-        public void AddPlayer(IPerson Person);
-        public int[,] Matrix { get; set; }
-        public int SIZE { get; set; }
-        public bool Check(int id, int x, int y);
-        public FStoneWayInfo[] IsSurrounded(int x, int y,int id, int[] Alt);
-    }
-    public enum EWays
-    {
-        OPEN,
-        FRIEND,
-        ALT
-    }
-    public struct FStoneWayInfo
-    {
-        public readonly EWays WayType;
-        public readonly int X;
-        public readonly int Y;
-
-        public FStoneWayInfo(EWays wayType, int x, int y)
-        {
-            WayType = wayType;
-            X = x;
-            Y = y;
-        }
-    }
-    public class GoRules:IRules
-    {
-        public List<IPerson> Persons;
-        private int ActivePersonIndex;
-        private int _size;
-        public bool bIsFinished;
-        
-
-        public void NextStep()
-        {
-            int Passes = 0;
-            foreach (IPerson Pers in Persons)
-            {
-                Passes += Pers.PassesCount;
-            }
-            if (Passes >= 2) bIsFinished = true;
-            if (!bIsFinished)
-            {
-                ActivePersonIndex++;
-                if (ActivePersonIndex >= Persons.Count) ActivePersonIndex = 0;
-            }
-            else
-            {
-                
-                
-                Console.WriteLine(Persons[0].Score.ToString()+" "+ Persons[1].Score.ToString());
-                //PlayerField.GetGame().MainFrame.Window.Close();
-                //TODO: post end game logic its fucking hard
-            }
-        }
-
-        public IPerson GetActivePerson()
-        {
-            return Persons[ActivePersonIndex];}
-
-        public void AddPlayer(IPerson Person)
-        {
-            Persons.Add(Person);
-        }
-
-        public int[,] Matrix { get; set; }
-        
         public int? this[int x, int y]
         {
-
             get
             {
                 if (x < 0 || x >= Matrix.GetLength(0) || y < 0 || y >= Matrix.GetLength(1)) return null; 
                 return Matrix[x, y];
             }
-            set=>  Matrix[x, y] =  (int)value;
-        }
-        public int SIZE
-        {
-            get => _size;
             set
             {
-                if (value!=_size)
-                {
-                    Matrix = new int[value,value];
-                }
-                _size = value;
-                
+                if (value != null) Matrix[x, y] = (int) value;
             }
         }
-
-        public GoRules(int size)
+        //public int XDim => Matrix.GetLength(0);
+        //public int YDim => Matrix.GetLength(1);
+        public abstract bool Check(int id, int x, int y);
+        public abstract StoneWayInfo[] IsSurrounded(int x, int y,int id, int[] alt);
+    }
+    public enum WayType
+    {
+        Open,
+        Friend,
+        Alt
+    }
+    public readonly struct StoneWayInfo
+    {
+        public readonly WayType WayTypeType;
+        public readonly int X;
+        public readonly int Y;
+        public StoneWayInfo(WayType wayTypeType, int x, int y)
         {
-            SIZE = size;
-            Persons = new List<IPerson>(2);
+            WayTypeType = wayTypeType;
+            X = x;
+            Y = y;
         }
-
-        public void SetMatrix(int[,] Matrix)
+    }
+    public class GoRulesBase:RulesBase
+    {
+        public readonly List<PersonBase> Persons;
+        private int _activePersonIndex;
+        public bool IsFinished;
+        public override void NextStep()
         {
-            this.Matrix = Matrix;
+            int passes = 0;
+            foreach (PersonBase pers in Persons)
+            {
+                passes += pers.PassesCount;
+            }
+            if (passes >= 2) IsFinished = true;
+            if (!IsFinished)
+            {
+                _activePersonIndex++;
+                if (_activePersonIndex >= Persons.Count) _activePersonIndex = 0;
+            }
+            else
+            {
+                Console.WriteLine(Persons[0].Score.ToString()+" "+ Persons[1].Score.ToString());
+                //TODO: post end game logic... its fucking hard
+            }
         }
-        
-        public bool Check(int id,int x, int y)
+        public PersonBase GetActivePerson()
         {
-            int? Element = this[x, y];
-            if (Element == null) return false;
-
-            if (Element == 0)
+            return Persons[_activePersonIndex];}
+        public override void AddPlayer(PersonBase personBase)
+        {
+            Persons.Add(personBase);
+        }
+        public GoRulesBase(int size):base(size)
+        {
+            _activePersonIndex = 0;
+            Persons = new List<PersonBase>(2);
+        }
+        public override bool Check(int id,int x, int y)
+        {
+            int? element = this[x, y];
+            if (element == null) return false;
+            if (element == 0)
             {
                 bool bSurrounded = false;
-
-                FStoneWayInfo[] WaysInfo = IsSurrounded(x, y, id, new int[]{});
-                if (WaysInfo.Length == 0) bSurrounded = true;
+                StoneWayInfo[] waysInfo = IsSurrounded(x, y, id, new int[]{});
+                if (waysInfo.Length == 0) bSurrounded = true;
                 if (!bSurrounded) this[x, y] = id;
                 return !bSurrounded;
             }
             return false;
-            //TODO: можно ли поставить проверяем при ходе в рулсах(КО)
-            //(самоубиственное, но можно забрать камни опонента и правило КО)
+            //TODO: "Ko" rule
         }
-
-        public FStoneWayInfo[] IsSurrounded(int x, int y, int id, int[] Alt)
+        public override StoneWayInfo[] IsSurrounded(int x, int y, int id, int[] alt)
         {
 
-            List<FStoneWayInfo> Answer = new List<FStoneWayInfo>();
-            int?[,] Around =
+            List<StoneWayInfo> answer = new List<StoneWayInfo>();
+            int?[,] around =
             {
                 {x,y+1},
                 {x,y-1},
                 {x+1,y},
                 {x-1,y}
             };
-            
-            bool Enemy = true;
-            for (int i = 0; i < Around.GetLength(0); i++)
+
+            for (int i = 0; i < around.GetLength(0); i++)
             {
-                int? Value = this[(int)Around[i,0],(int)Around[i,1]];
-                if (Value == 0) Answer.Add(new FStoneWayInfo(EWays.OPEN, (int) Around[i, 0], (int) Around[i, 1]));  
-                else if (Value == id) Answer.Add(new FStoneWayInfo(EWays.FRIEND, (int) Around[i, 0], (int) Around[i, 1]));
+                int? value = this[(int)around[i,0],(int)around[i,1]];
+                if (value == 0) answer.Add(new StoneWayInfo(WayType.Open, (int) around[i, 0], (int) around[i, 1]));  
+                else if (value == id) answer.Add(new StoneWayInfo(WayType.Friend, (int) around[i, 0], (int) around[i, 1]));
                 else
                 {
-                    foreach (int Var in Alt)
+                    foreach (int var in alt)
                     {
-                        if(Value == Var) Answer.Add(new FStoneWayInfo(EWays.ALT, (int) Around[i, 0], (int) Around[i, 1]));  
+                        if(value == var) answer.Add(new StoneWayInfo(WayType.Alt, (int) around[i, 0], (int) around[i, 1]));  
                     }
                 }
             }
-            return Answer.ToArray();
+            return answer.ToArray();
         }
-
-        public void Print()
+        public void PrintMatrix()
         {
-            for (int i = 0; i < Matrix.GetLength(0); i++)
+            for (int i = 0; i < this[0]; i++)
             {
-                for (int j = 0; j<Matrix.GetLength(1); j++)
+                for (int j = 0; j<this[1]; j++)
                 {
-                    if (Matrix[i, j] < 0)
-                        Console.Write(Matrix[i, j]);
-                    else Console.Write(" "+ Matrix[i, j]);
+                    if (this[i, j] < 0)
+                        Console.Write(this[i, j]);
+                    else Console.Write(" "+ this[i, j]);
                 }
                 Console.Write('\n');
             }
@@ -355,10 +282,9 @@ namespace Game
     public class PlayerField
     {
         private PlayerField() { }
-        private static PlayerField _instance;
-
-        public Graphics MainFrame { get; set; }
-        public IRules Rules { get; set; }
+        private static PlayerField? _instance;
+        public Graphics MainFrame { get; private set; }
+        private RulesBase RulesBase { get; set; }
         public static PlayerField GetGame()
         {
             if (_instance == null) {
@@ -367,29 +293,24 @@ namespace Game
             }
             return _instance;
         }
-
-        public void StartGame(int SIZE)
+        public void StartGame(int size)
         {
-            GetGame().Rules = new GoRules(SIZE);
+            GetGame().RulesBase = new GoRulesBase(size);
             
-            Player FirstPlayer = new Player(Rules,1);
-            Player SecondPlayer = new Player(Rules,2);
+            Player firstPlayer = new Player(RulesBase,1);
+            Player secondPlayer = new Player(RulesBase,2);
             
-            Goban Go = new Goban((GoRules)GetGame().Rules);
-            GetGame().MainFrame.AddGroup(Go);
-            
+            Goban goGame = new Goban((GoRulesBase)GetGame().RulesBase);
+            GetGame().MainFrame.AddGroup(goGame);
             GetGame().MainFrame.Update();
         }
-        
     }
-    
-    
-    class Program
+    static class Game
     {
         static void Main(string[] args)
         {
-            PlayerField Field = PlayerField.GetGame();
-            Field.StartGame(9);
+            PlayerField field = PlayerField.GetGame();
+            field.StartGame(9);
         }
     }
 }
--- NUnitTest/UnitTest1.cs
+++ NUnitTest/UnitTest1.cs
@@ -1,40 +1,27 @@
-using System;
-using System.Data;
+
 using NUnit.Framework;
-using Game;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-using NuGet.Frameworks;
+using Game.Logic;
+
+
 
 namespace NUnitTest
 {
     public class Tests
     {
-        public GoRules Rules;
+        public GoRulesBase RulesBase;
         public Player player;
         public Player player2;
-
-        
         [SetUp]
         public void Setup()
         {
-            
-
-            Rules = new GoRules(9);
-            player = new Player(Rules);
-            //BigBrother ChangeStone = new ChangeStoneState();
-            //player.AddObserver(ChangeStone);
-            player2 = new Player(Rules,2);
-            //player2.AddObserver(new ChangeStoneState());
-
+            RulesBase = new GoRulesBase(9);
+            player = new Player(RulesBase);
+            player2 = new Player(RulesBase,2);
         }
-        
-        
-        
         [Test]
         public void Compare_Reference_To_Rules()
         {
-            
-            Assert.IsTrue(ReferenceEquals(Rules,player.Rules));
+            Assert.IsTrue(ReferenceEquals(RulesBase,player.RulesBase));
         }
         [Test]
         public void Make_Wrong_Step()
@@ -87,54 +74,34 @@ namespace NUnitTest
             player.MakeStep(0, 1);
             player2.MakeStep(1, 1);
             player.MakeStep(2, 1);
-            Rules.Print();
+            RulesBase.PrintMatrix();
             player.MakeStep(1, 2);
-            Rules.Print();
-            //Assert.AreEqual(-1,Rules.Matrix[1,1]);
+            RulesBase.PrintMatrix();
             Assert.AreEqual(1,player.Score);
         }
 
         [Test]
         public void Field2()
         {
-            Rules.SIZE = 7;
-            Rules.SetMatrix(new int[7,7]
+            RulesBase.Matrix = new int[7, 7]
             {
-                {0,1,1,1,1,0,0},
-                {1,0,2,2,2,1,0},
-                {1,1,1,1,1,1,0},
-                {0,2,2,1,2,2,1},
-                {0,0,0,0,1,1,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0}
-            });
-            player.OcupateEnemy();
-            Rules.Print();
-            Assert.AreEqual(-4,Rules.Matrix[1,2]);
-            Assert.AreEqual(-4,Rules.Matrix[1,3]);
-            Assert.AreEqual(-4,Rules.Matrix[1,4]);
-            Assert.AreEqual(-4,Rules.Matrix[3,1]);
-            Assert.AreEqual(-4,Rules.Matrix[3,2]);
-            Assert.AreEqual(-1,Rules.Matrix[3,4]);
-            Assert.AreEqual(-1,Rules.Matrix[3,5]);
+                {0, 1, 1, 1, 1, 0, 0},
+                {1, 0, 2, 2, 2, 1, 0},
+                {1, 1, 1, 1, 1, 1, 0},
+                {0, 2, 2, 1, 2, 2, 1},
+                {0, 0, 0, 0, 1, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0}
+            };
+            player.OccupyEnemy();
+            RulesBase.PrintMatrix();
+            Assert.AreEqual(-4,RulesBase[1,2]);
+            Assert.AreEqual(-4,RulesBase[1,3]);
+            Assert.AreEqual(-4,RulesBase[1,4]);
+            Assert.AreEqual(-4,RulesBase[3,1]);
+            Assert.AreEqual(-4,RulesBase[3,2]);
+            Assert.AreEqual(-1,RulesBase[3,4]);
+            Assert.AreEqual(-1,RulesBase[3,5]);
         }
-
-
-        [Test]
-        public void Test_Match()
-        {
-            
-            
-            Assert.Pass();
-            //player.MakeStep()
-        }
-
-        [Test]
-        public void TestBug()
-        {
-            ((GoRules) Rules)[0, 8] = 3;
-            Assert.Pass();
-        }
-        
     }
 }
